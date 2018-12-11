cf8797768c7d111e80906379386076899d2cf827
马龙 <malong94@163.com>
2018-12-11 15:42:27 +0800

    This is studt googalMap Day2 and this is include little tital and searce

--- googleMap/routes/index.js
+++ googleMap/routes/index.js
@@ -8,5 +8,11 @@ router.get('/', function(req, res, next) {
 router.get('/demo', function(req, res, next) {
   res.render('demo', { title: 'demo' });
 });
+router.get('/choiceCity', function(req, res, next) {
+  res.render('choiceCity', { title: '城市选择' });
+});
 
+router.get('/searce', function(req, res, next) {
+  res.render('searce', { title: '定点' });
+});
 module.exports = router;
